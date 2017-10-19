e4bd0b290f0a73b50fd97f24152a508edd05ea71
Vincent Letourneau <vincent@nanoninja.com>
2017-10-19 12:45:16 +0200

    Correction de l'éxercice Todo List

--- hello/src/app/todo/todo.component.ts
+++ hello/src/app/todo/todo.component.ts
@@ -16,9 +16,25 @@ export class TodoComponent implements OnInit {
   ngOnInit() { }
 
   addTask(event: KeyboardEvent) {
-    if (event.keyCode === 13) {
+    if (event.keyCode === 13 && this.taskText.trim()) {
       this.tasks.push(new Task(this.taskText, false));
       this.taskText = '';
     }
+  }
+
+  removeTask(index): void {
+    this.tasks.splice(index, 1);
+  }
+
+  getNbTaskDone(): number {
+    let total = 0;
+
+    for (let task of this.tasks) {
+      if (task.done === true) {
+        total++;
+      }
+    }
+
+    return total;
   }
 }
